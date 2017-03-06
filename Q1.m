22b04bee6c77678115dd15497706d7b77e270900
Yucheng Lin <Pike96@users.noreply.github.com>
2017-03-06 14:01:57 -0800

    Formatting

--- Q1.m
+++ Q1.m
@@ -1,16 +1,17 @@
 clear;
 
-Tt = 0;
-Ts = 0;
+Tt = 0;     % arrival time sequence
+Ts = 0;     % time of 1st arrival after s
 i = 0;
-lambda = 20;
-t = 0;
-s = 0;
+lambda = 20;    % customized lambda constant
+s = 0;      % time past
 
+% Generate arrival time sequence
 while Ts <= 100
     t = s;
     while 1
         t = t - log(rand)/lambda;
+        % Generate lambda_t using modulo
         if mod(t,10) < 5
             lambda_t = mod(t,10)*3 + 4;
         else
@@ -41,12 +42,13 @@ while tA<100
         tA = Tt(i);
         n = n+1;
         if n == 1
-            tD = t+exprnd(1/25);
+            tD = t+exprnd(1/25);    % Exp distri. with rate 25
         end
-    else
+    else    % tA > tD
         t = tD;
         n = n-1;
         if n == 0
+            % Break time
             tbc = 0;    % accumulated continuous break time
             while t + tbc < tA
                 tb = 0.3*rand;
@@ -59,3 +61,5 @@ while tA<100
         end
     end
 end
+
+disp(['The expected total break time in 100 hrs is: ',num2str(tbtotal)]);
